262d512f023a579b71c5877c61504a4385633548
Oleksandr <oleksandr.moskaliuk@logivations.com>
2021-12-14 18:08:22 +0200

    Revert "-> gazebo_ros_realsense.h manually formatted"
    This reverts commit 94f61d94

--- include/realsense_gazebo_plugin/gazebo_ros_realsense.h
+++ include/realsense_gazebo_plugin/gazebo_ros_realsense.h
@@ -13,43 +13,52 @@
 #include <memory>
 #include <string>
 
-namespace gazebo {
+namespace gazebo
+{
 /// \brief A plugin that simulates Real Sense camera streams.
 class GazeboRosRealsense : public RealSensePlugin
 {
   /// \brief Constructor.
+
 public:
   GazeboRosRealsense();
 
   /// \brief Destructor.
+
 public:
   ~GazeboRosRealsense();
 
   // Documentation Inherited.
+
 public:
   virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
 
   /// \brief Callback that publishes a received Depth Camera Frame as an
   /// ImageStamped message.
+
 public:
   virtual void OnNewDepthFrame();
 
   /// \brief Helper function to fill the pointcloud information
-  bool FillPointCloudHelper(sensor_msgs::msg::PointCloud2 & point_cloud_msg, uint32_t rows_arg,
-                            uint32_t cols_arg, uint32_t step_arg, void * data_arg);
+  bool FillPointCloudHelper(
+    sensor_msgs::msg::PointCloud2 & point_cloud_msg, uint32_t rows_arg,
+    uint32_t cols_arg, uint32_t step_arg, void * data_arg);
 
   /// \brief Callback that publishes a received Camera Frame as an
   /// ImageStamped message.
+
 public:
-  virtual void OnNewFrame(const rendering::CameraPtr cam,
-                          const transport::PublisherPtr pub);
+  virtual void OnNewFrame(
+    const rendering::CameraPtr cam,
+    const transport::PublisherPtr pub);
 
 protected:
   boost::shared_ptr<camera_info_manager::CameraInfoManager>
-      camera_info_manager_;
+  camera_info_manager_;
 
   /// \brief A pointer to the ROS node.
   ///  A node will be instantiated if it does not exist.
+
 protected:
   rclcpp::Node::SharedPtr node_;
 
@@ -62,6 +71,7 @@ protected:
   image_transport::CameraPublisher color_pub_, ir1_pub_, ir2_pub_, depth_pub_;
 
   /// \brief ROS image messages
+
 protected:
   sensor_msgs::msg::Image image_msg_, depth_msg_;
   sensor_msgs::msg::PointCloud2 pointcloud_msg_;
